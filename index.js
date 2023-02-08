7ba80b883055e26379c0578d03abea93f0ffb7a8
Jhonattanr <camicasii@hotmail.com>
2023-02-08 18:48:58 -0500

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const  mongoose = require('mongoose');
 require('dotenv').config()
 
 app.use(cors())
@@ -10,54 +11,71 @@ app.use(express.urlencoded({ extended: true }))
 app.get('/', (req, res) => {  
   res.sendFile(__dirname + '/views/index.html')
 });
-const users = [];
-const exercises = []
+// const users = [];
+// const exercises = []
 
-app.post('/api/users', (req, res) => {
+let userSchema = new mongoose.Schema({
+  username: String
+});
+let User = mongoose.model("User", userSchema);
+let exerciseSchema = new mongoose.Schema({
+  username: String,
+  description: String,
+  duration: Number,
+  date: Date,
+});
+let Exercise = mongoose.model("Exercise", exerciseSchema);
+
+
+app.post('/api/users', async (req, res) => {
   const { username } = req.body;
-  users.map((user) => {
-    if (user.username === username) {
-      return res.json({ error: "Username already taken" });
-    }
-  });
-  users.push({ username: username, _id: users.length });
-  res.json({ username: username, _id: users.length - 1 });
-
+  const user =User.find({username:username});
+  if (user) {
+    return res.json({ error: "Username already taken" });
+  }
+  const newUser = new User({username:username});
+  await newUser.save();
+  res.json({ username: username, _id: newUser._id });
 });
 app.post('/api/users/:_id/exercises', (req, res) => {
   const { _id } = req.params;
   const { description, duration, date } = req.body;
-  const user = users[Number(_id)];
+  const user =User.findById(_id);
   if (!user) {
     res.json({ error: "User not found" });
   }
   else {
-    const exercise = {
-      _id,      
+    const exercise = new Exercise({   
+      username: user.username,       
       description: description,
       duration: Number(duration),
       date: date ? new Date(date).toDateString() : new Date().toDateString()
-    };
-    exercises.push(exercise);
-    const check = exercises.filter(user_ => user_._id == _id).map(exercise_ => {
-      if(!!exercise_)
-      return Object.assign({username:users[_id].username}, exercise_)
-    })
-    check.concat(Object.assign({username:users[_id].username}, exercise));
-    return res.json(
-      check
-    );
+    });
+    exercise.save();
+    return res.json(exercise);
+  }
       
-    res.json(
-    {
-      username: user.username,
-      description: exercise.description,
-      duration: exercise.duration,      
-      date: exercise.date,
-      _id: exercise._id
 
-    }
-    );
+    
+    // const check = exercises.filter(user_ => user_._id == _id).map(exercise_ => {
+    //   if(!!exercise_)
+    //   return Object.assign({username:users[_id].username}, exercise_)
+    // })
+    // check.concat(Object.assign({username:users[_id].username}, exercise));
+    // return res.json(
+    //   check
+    // );
+      
+    // res.json(
+    // {
+    //   username: user.username,
+    //   description: exercise.description,
+    //   duration: exercise.duration,      
+    //   date: exercise.date,
+    //   _id: exercise._id
+
+    // }
+    // );
     // const check = exercises.filter(user => user._id == _id).map(exercise_ => {
     //   if(!!exercise_)
     //   return Object.assign({username:users[_id].username}, exercise_)
@@ -65,46 +83,30 @@ app.post('/api/users/:_id/exercises', (req, res) => {
     // console.log(check);
     // console.log(exercise); 
     // res.json( Object.assign({username:users[_id].username}, exercise));
-  }
-});
-app.get('/api/users/:_id/exercises', (req, res) => {
-  const { _id } = req.params;
-  const user = users[Number(_id)];
-  if (!user) {
-    res.json({ error: "User not found" });
-  }
-  else {
-    
-    const check = exercises.filter(user_ => user_._id == _id).map(exercise_ => {
-      if(!!exercise_)
-      return Object.assign({username:users[_id].username}, exercise_)
-    })
-    res.json(check);
-  }
+  // }
 });
 app.get('/api/users', (req, res) => {
+  const users =User.find();
   res.json(
 users
-    // users.map(user => {
-    //   return { username: user.username, _id: user._id }
-    // })
-  );
+      );
 });
 
 app.get('/api/users/:_id/logs', (req, res) => {
   const { _id } = req.params;
   const { from, to, limit } = req.query;
-  const user = users[Number(_id)];
+  const user =User.findById(_id);
+  const exercises =Exercise.find({username:user.username});
   if (!user) {
     res.json({ error: "User not found" });
   }
   else {
-    let result = exercises.filter(user => user._id == _id);
+    let result = exercises;
     if (from) {
-      result = result.filter(user => new Date(user.date) > new Date(from));
+      result = result.filter(user_ => new Date(user_.date) > new Date(from));
     }
     if (to) {
-      result = result.filter(user => new Date(user.date) < new Date(to));
+      result = result.filter(user_ => new Date(user_.date) < new Date(to));
     }
     if (limit) {
       result = result.slice(0, limit);
@@ -119,5 +121,6 @@ app.get('/api/users/:_id/logs', (req, res) => {
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
+  mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
   console.log('Your app is listening on port ' + listener.address().port)
 })
