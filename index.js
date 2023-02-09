0741da79f0fbe16025ab805d473a2f6391aa0b8f
Jhonattanr <camicasii@hotmail.com>
2023-02-08 21:04:03 -0500

    update

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ app.get("/api/users/:_id/logs", async (req, res) => {
           return {
             description: String(user_.description),
             duration: Number(user_.duration),
-            date: user_.date,
+            date:  new Date().toDateString()
             };
         });
 
